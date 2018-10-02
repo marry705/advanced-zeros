c28fd3e42b85ec4bedfb0d5f66ed99569bbe5b5c
Marry705 <marry705chilo@gmail.com>
2018-10-02 20:06:25 +0300

    therd

--- src/index.js
+++ src/index.js
@@ -3,45 +3,14 @@
 * @ return how many zeros in the end of number, 
 * which is factorial of number in base base system
 */
-/*module.exports = function getZerosCount(number = 1, base = 2) {
-    let j = 0;
-    let i = 2;
-    let arr = [];
-    let bas = base;
-    let num = number;
-    while (i < bas){
-        if (bas % i === 0){
-            arr[j] = i;
-            j++;
-            bas = bas/i;
-        }
-        else i++;
-    }
 
-    arr[j] = i;
-
-    let len = arr.length;
-    let last = arr[len-1];
-
-    let rep = 0;
-    for(j = 0; j < len; j++){
-        if (arr[j] === last) rep++;
-    }
-
-    let count = 0;
-    while(num){
-        num = (num/last)|0;
-        count+=num;
-    }
-    return Math.floor(count/rep);
-}*/
 module.exports = function getZerosCount(number = 1, base = 2) {
     let k = 0;
     let j = 0;
     let i = 2;
     let count = 0;
-    let numbersB = [];
-    let exp = [];
+    const numbersB = [];
+    const exp = [];
     let bas = base;
     
     while(i <= bas){
@@ -64,7 +33,7 @@ module.exports = function getZerosCount(number = 1, base = 2) {
     const len = numbersB.length;
     count = 0;
     j = 0;
-    let zer = [];
+    const zer = [];
     let num = number;
     for (i = 0; i < len; i++){
         k = numbersB[i];
